08c0447b086de22de0331356303d687d8efb7ff4
fikrikemalakbar <fikrisangmaaestromidfielder@gmail.com>
2021-08-02 12:25:50 +0700

    slider

--- front/js/main.js
+++ front/js/main.js
@@ -9,3 +9,24 @@ function closeNav() {
   document.getElementById("mainoffcanvas").style.marginLeft= "0";
 }
 
+// slider
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" activeSlider", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " activeSlider";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
