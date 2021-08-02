50b10fac905f68cb860d376b0193d10131bc7c31
fikrikemalakbar <fikrisangmaaestromidfielder@gmail.com>
2021-08-02 11:49:23 +0700

    navbar

--- front/js/main.js
+++ front/js/main.js
@@ -1,9 +1,11 @@
+// menu header close navigation menu mobile
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("header-menu").style.marginLeft = "250px";
+  document.getElementById("mainoffcanvas").style.marginLeft = "250px";
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("header-menu").style.marginLeft= "0";
+  document.getElementById("mainoffcanvas").style.marginLeft= "0";
 }
+
